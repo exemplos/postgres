868d96ecabee7e49fa83af73b598b791b9a9c5ed
Francisco Calaça <chicocx@gmail.com>
2018-11-06 11:10:15 -0200

    Create item_bau.sql

---
+++ item_bau.sql
@@ -0,0 +1,27 @@
+
+CREATE TABLE public.item_bau (
+    id serial NOT NULL,
+    item_id integer,
+    bau_id integer,
+    quantidade integer
+);
+
+
+
+INSERT INTO public.item_bau (id, item_id, bau_id, quantidade) VALUES (1, 1, 1, 3);
+INSERT INTO public.item_bau (id, item_id, bau_id, quantidade) VALUES (2, 3, 1, 5);
+INSERT INTO public.item_bau (id, item_id, bau_id, quantidade) VALUES (3, 4, 1, 30);
+INSERT INTO public.item_bau (id, item_id, bau_id, quantidade) VALUES (4, 2, 2, 5);
+INSERT INTO public.item_bau (id, item_id, bau_id, quantidade) VALUES (5, 1, 2, 1);
+
+
+
+SELECT pg_catalog.setval('public.item_bau_id_seq', 5, true);
+
+
+
+
+ALTER TABLE ONLY public.item_bau
+    ADD CONSTRAINT item_bau_pkey PRIMARY KEY (id);
+
+	
